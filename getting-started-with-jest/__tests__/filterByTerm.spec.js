6ce204c50f01258b52eef6f11b35f59ccbaf3a14
tiagov29 <santiagovalenciav29@gmail.com>
2021-11-29 20:00:05 -0500

    testing empty completed

--- getting-started-with-jest/__tests__/filterByTerm.spec.js
+++ getting-started-with-jest/__tests__/filterByTerm.spec.js
@@ -1,5 +1,7 @@
+const filterByT = require("../src/filterByTerm");
+
 describe("Filter function", () => {
-    test("it should find by a search term (uRl)", () => {
+    test("it should filter by a search term (link)", () => {
         const input = [
             {id: 1, url: "http://www.url1.dev"},
             {id: 2, url: "http://www.url2.dev"},
@@ -16,6 +18,56 @@ describe("Filter function", () => {
 });
 
 function filterByTerm(inputArr, searchTerm) {
+    const regex = new RegExp(searchTerm, "i");
+    return inputArr.filter(function(arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
+
+
+// test for search term "url"
+
+describe("find term uRL", () => {
+    test("it should filter by a search term (uRL)", () => {
+        const input = [
+            {id: 1, url: "http://www.url1.dev"},
+            {id: 2, url: "http://www.url2.dev"},
+            {id: 3, url: "http://www.link3.dev"}
+        ];
+
+        const output = [{ id:1, url: "http://www.url1.dev"},{id: 2, url: "http://www.url2.dev"}];
+
+        expect(findByTerm(input, "uRL")).toEqual(output);
+    })
+});
+
+
+function findByTerm(inputArr, searchTerm) {
+    const regex = new RegExp(searchTerm, "i");
+    return inputArr.filter(function(arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
+
+
+//test for an empty search term
+
+describe("find an empty search term", () => {
+    test("it should find a empty search term (obs)", () => {
+        const input = [
+            {id: 1, url: "http://www.url1.dev"},
+            {id: 2, url: "http://www.url2.dev"},
+            {id: 3, url: "http://www.link3.dev"}
+        ];
+
+        const output = [];
+
+        expect(ByTerm(input, "obs")).toEqual(output);
+    })
+});
+
+
+function ByTerm(inputArr, searchTerm) {
     const regex = new RegExp(searchTerm, "i");
     return inputArr.filter(function(arrayElement) {
         return arrayElement.url.match(regex);
